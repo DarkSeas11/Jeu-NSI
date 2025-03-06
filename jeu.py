3a8862f071f524b088f625f90755fb88aa495b1f
DarkSeas11 <135517543+DarkSeas11@users.noreply.github.com>
2025-03-06 10:19:58 +0000

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -7,8 +7,22 @@ pygame.display.set_caption("MY GAME")
 fond_ecran=pygame.image.load('mario back.jpg')
 
 #création personnage
+x_pers=x_ecran/2-45
+y_pers=y_ecran-148
 pers=pygame.image.load('character.png')
-rect_pers=pygame.Rect(x_ecran/2-45,y_ecran-100,118,118)
+rect_pers=pygame.Rect(x_ecran/2-45,y_ecran-148,118,118)
+
+#jump
+speed=1
+gravity=1
+jump=False
+
+#création sol
+x=0
+x_sol=0
+y_sol=y_ecran-48
+sol=pygame.image.load('sol mario.png')
+rect_sol=pygame.Rect(x_sol+x,y_sol,48,48)
 
 #boucle jeu
 run = True
@@ -19,25 +33,38 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.key==pygame.K_SPACE and jump==False:#double jump tirar "jump==False"
+                    speed-=20
     #keys
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         run = False
     if keys[pygame.K_RIGHT]:
-        rect_pers.left+=1
+        rect_pers.left+=2
     if keys[pygame.K_LEFT]:
-        rect_pers.left-=1
+        rect_pers.left-=2
     if keys[pygame.K_SPACE]:
-        rect_pers.bottom-=3
-        for i in range(3):
-            rect_pers.bottom+=1
-        
+        rect_pers.bottom-=10
+    elif rect_pers.bottom<=rect_sol.top-10:
+        rect_pers.bottom=rect_sol.top    
     #dessins
-    screen.blit(pers,rect_pers)    
-        
+    screen.blit(pers,rect_pers)
+    
+    screen.blit(sol,rect_sol)
+    
+    #mouvements
+    y_pers+=speed
+    if y_pers>=x_ecran-(rect_sol.top-48):
+        speed=0
+        jump=False
+    else:
+        jump=True
+        speed+=gravity
+    
     # mise à jour de l´écran
     pygame.display.update()
 
 # On sort de la boucle et on quitte
 pygame.quit()
+
 
