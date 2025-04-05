7d450fffb8fa1aa9beea79f64386e69fb71e94d4
DarkSeas11 <135517543+DarkSeas11@users.noreply.github.com>
2025-04-05 18:29:49 +0100

    Update code lourenco jeu.py

--- code lourenco jeu.py
+++ code lourenco jeu.py
@@ -22,8 +22,8 @@ sondefond=pygame.mixer.music.load("sondefond.mp3")
 pygame.mixer.music.play()
 
 #création personnage
-x_pers=x_ecran/2
-y_pers=y_ecran-160
+x_pers=0
+y_pers=y_ecran-156
 pers=pygame.image.load('character.png')
 rect_pers=pygame.Rect(x_pers,y_pers,118,118)
 dir=1
@@ -33,35 +33,37 @@ speed=0
 gravity=2
 jump=False
 
-#création sol
-x_sol=0
-y_sol=y_ecran-50
-sol=pygame.image.load('sol mario.png')
-liste_x=[]
-x_liste=0
-y_liste=y_sol
-x=0
-avance=0
-for f in range(250):
-    liste_x.append(x_liste)
-    x_liste+=50
-
 # Camera offset
 camera_x = 0
 
+#création sol
+x_sol=0
+y_sol=y_ecran-48
+sol=pygame.image.load('sol mario.png')
+liste_x=[]
+x_liste=0
+
+rect_sol2=pygame.Rect(600,y_sol-48,48,48)
+for f in range(250):
+    liste_x.append(x_liste)
+    x_liste+=48
+    
+
+#Création Tubes
+tube=pygame.image.load('pipe 3px.png')
+x_tube=1440
+y_tube=y_ecran-144
+rect_tube1=pygame.Rect(x_tube,y_tube,144,101)
+
 # Niveau 1
 def niveau1(camera_x):
-    for i in range(len(liste_x)):
-        screen.blit(sol, (liste_x[i] - camera_x, y_sol))
-        screen.blit(sol, (liste_x[i]+1000 - camera_x, y_sol-50))
-        screen.blit(sol, (liste_x[i]+2000 - camera_x, y_sol-100))
+    pass
 
 #boucle jeu
 run = True
 while run:
     #fond ecran
     screen.blit(fond_ecran,(0,0))
-    
     #event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -75,22 +77,26 @@ while run:
         run = False
         
     if keys[pygame.K_RIGHT]:
+        print(rect_pers.right)
         dir=1
-        if rect_pers.right>=x_ecran-250:
+        #Collisions
+        if pygame.Rect.colliderect(rect_pers,rect_sol1):
             rect_pers.right+=0
-            camera_x += 5
+            camera_x+=0
         else:
             rect_pers.right+=3
-            camera_x += 5
+            camera_x += 3
+        
             
     if keys[pygame.K_LEFT]:
         dir=-1
+        
         if rect_pers.left<=250:
             rect_pers.right+=0
-            camera_x -= 5
+            camera_x -= 3
         else:
             rect_pers.right-=3
-            camera_x -= 5
+            camera_x -= 3
             
     if keys[pygame.K_SPACE] and jump==False:
         speed-=20
@@ -102,12 +108,17 @@ while run:
     elif dir==-1:
         screen.blit(pygame.transform.flip(pers,True,False),rect_pers)
     
+    screen.blit(tube,rect_tube1)
+    
         #niveau 1
-    niveau1(camera_x)
+    for i in range(len(liste_x)):
+        screen.blit(sol, (liste_x[i]-camera_x, y_sol))
+        screen.blit(sol, (liste_x[i]+960-camera_x, y_sol-48))
+        screen.blit(sol, (liste_x[i]+1200-camera_x, y_sol-96))
     
     #mouvements
     rect_pers.bottom+=speed
-    if rect_pers.bottom>=y_ecran-50:
+    if rect_pers.bottom>=y_sol:
         speed=0
         jump=False
     else:
